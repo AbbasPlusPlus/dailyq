af5e4cbeda06d6f95fba1a8e4633769a75c9606f
Mohammad Abbas <mabbasplusplus@gmail.com>
2024-02-03 18:50:08 +0000

    fixed sign in with google component not redirecting

--- src/components/signInWithGoogle/signInWithGoogle.jsx
+++ src/components/signInWithGoogle/signInWithGoogle.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import { GoogleIcon, StyledButton } from "./signInWithGoogle.styles";
 
 export const SignInWithGoogle = ({ signup }) => {
   const { signupWithGoogle } = useAuth();
+  const navigate = useNavigate();
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await signupWithGoogle();
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <StyledButton onClick={signupWithGoogle} className="d-block mb-3">
+    <StyledButton onClick={handleSignInWithGoogle} className="d-block mb-3">
       <GoogleIcon /> {`Sign ${signup ? "up" : "in"} with Google`}
     </StyledButton>
   );
